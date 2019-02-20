1076a67da942c16fa15635d5d04e1df8cd99a210
sangeethasangee <47592800+sangeethasangee@users.noreply.github.com>
2019-02-20 14:01:13 +0530

    Create number.c

---
+++ number.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<conio.h>
+
+int main(void) 
+{
+	int a;
+	scanf("%d",&a);
+	if(a>0 && a<0)
+	{
+		printf("Positive");
+	}
+	elseif
+	{
+		printf("negative");
+	}
+  else
+  {
+  printf("zero");
+  }
+  	return 0;
+    }
